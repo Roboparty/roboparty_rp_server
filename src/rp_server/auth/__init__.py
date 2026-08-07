01cfb779c7cfc0e8d47c304fa7fb4a28f8de1569
mustaf-osman <mustaf-osman@users.noreply.github.com>
2026-08-07 15:18:03 +0800

    chore: remove auth module files from repo

--- src/rp_server/auth/__init__.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0
-# Copyright (C) 2026 mustaf-osman (https://github.com/mustaf-osman)
-# Copyright (C) 2026 wentywenty (https://github.com/wentywenty)
-
-from .jwt_util import decode_jwt, encode_jwt
-from .store import AuthStore, QrChallenge
-
-__all__ = ["AuthStore", "QrChallenge", "encode_jwt", "decode_jwt"]
